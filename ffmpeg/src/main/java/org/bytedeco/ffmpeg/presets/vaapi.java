042d4589d63847586625c2cd2f3fb56f96d092b3
Silas Della Contrada <me@silenium-dev.net>
2025-03-25 21:16:26 +0100

    fix(ffmpeg): link against va, va-drm and drm

--- ffmpeg/src/main/java/org/bytedeco/ffmpeg/presets/vaapi.java
+++ ffmpeg/src/main/java/org/bytedeco/ffmpeg/presets/vaapi.java
@@ -19,13 +19,15 @@ import org.bytedeco.javacpp.tools.InfoMapper;
             value = "linux-x86_64",
             define = {"__STDC_CONSTANT_MACROS", "__STDC_FORMAT_MACROS", "STRING_BYTES_CHARSET \"UTF-8\""},
             cinclude = {"<va/va.h>", "<va/va_drm.h>", "<va/va_drmcommon.h>", "<va/va_version.h>", "<va/va_str.h>"},
-            includepath = {"/usr/include/"}
+            includepath = {"/usr/include/"},
+            link = {"va@.2", "va-drm@.2", "drm@.2"}
         ),
         @Platform(
             value = "linux-arm64",
             define = {"__STDC_CONSTANT_MACROS", "__STDC_FORMAT_MACROS", "STRING_BYTES_CHARSET \"UTF-8\""},
             cinclude = {"<va/va.h>", "<va/va_drm.h>", "<va/va_drmcommon.h>", "<va/va_version.h>", "<va/va_str.h>"},
-            includepath = {"/usr/include/"}
+            includepath = {"/usr/include/"},
+            link = {"va@.2", "va-drm@.2", "drm@.2"}
         ),
         @Platform(extension = "-gpl"),
     }
